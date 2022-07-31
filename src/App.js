df38887dc8c4e96108ee8e48e89ab5296a337040
Daniel Villacis <daniel1997v@gmail.com>
2022-07-31 02:35:54 -0400

    AddPatient finished and tested. Downgraded react-toastify to @8.2.0

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import CollectClinicals from './components/CollectClinicals';
+import AddPatient from './components/AddPatient';
+import AnalyzeData from './components/AnalyzeData';
+import ChartGenerator from './components/ChartGenerator';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
-
+      <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/patientDetails/:patientId" component={CollectClinicals}/>
+          <Route exact path="/addPatient" component={AddPatient} />
+          <Route exact path="/analyze/:patientId" component={AnalyzeData} />
+          <Route exact path="/chart/:componentName/:patientId" component={ChartGenerator} />
+      </Switch>
     </div>
   );
 }
